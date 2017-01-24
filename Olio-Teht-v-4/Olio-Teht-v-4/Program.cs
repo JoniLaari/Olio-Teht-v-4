91a63ef86deb9bb38fffa762df845f94312016c4
h8324 <h8324@MEDI-426-WS-20>
2017-01-24 19:39:47 +0200

    Initial commit.

---
+++ Olio-Teht-v-4/Olio-Teht-v-4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ikä;
+            Console.Write("Anna ikäsi > ");
+            ikä = int.Parse(Console.ReadLine());
+            if (ikä < 18) Console.WriteLine("Alaikäban");
+            if (ikä >= 18 && ikä <= 65) Console.WriteLine("Aikuinen");
+            if (ikä > 65) Console.WriteLine("Seniori");
+        }
+    }
+}
